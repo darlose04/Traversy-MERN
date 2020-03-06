b4c217d576a3ab5ddbcdff92e7c008aba3265efa
Zach Smith <zsmith126028@gmail.com>
2020-03-06 10:23:16 -0600

    destructure props

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { setAlert } from "../../actions/alert";
 
-const Register = props => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -22,7 +22,7 @@ const Register = props => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      props.setAlert("Passwords do not match", "danger");
+      setAlert("Passwords do not match", "danger");
     } else {
       console.log("Success!");
     }
