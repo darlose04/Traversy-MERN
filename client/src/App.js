6a4ab8e285ee56f4564194bcf26f226b22e4d21b
Zach Smith <zsmith126028@gmail.com>
2020-03-05 21:53:45 -0600

    add components, import router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
 
 const App = () => (
   <Fragment>
-    <h1>Client Side</h1>
+    <Navbar />
+    <Landing />
   </Fragment>
 );
 
